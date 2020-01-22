3e726ad9d1144ab9581c55ad7a3262f8e8423dc9
pooyafekri79@gmail.com <po.fe.09134419188>
2020-01-22 21:24:02 +0330

    Some page that created before

---
+++ signup.js
@@ -0,0 +1,46 @@
+new Vue({
+  el: '#myForm',
+  data: {
+      firstName: '',
+      lastName: '',
+      email:'',
+      hashedPassword:'',
+      hashedPassword2: '',
+      username:'',
+      unmatchedPass: true
+  },
+  methods: {
+      logName: function(){
+          alert(this.firstName);
+      },
+      logLastName: function(){
+          alert(this.lastName);
+      },
+      checkpasswd: function(){
+        if(this.hashedPassword != this.hashedPassword2)
+          this.unmatchedPass = true;
+        else
+          this.unmatchedPass = false;
+      },
+      submit:function(){
+        axios.post('http://localhost:8080/api/v1/users', {
+          firstName: this.firstName,
+          lastName: this.lastName,
+          email: this.email,
+          hashedPassword: this.hashedPassword,
+          username: this.username
+        },
+        { headers: {
+          'Content-type': 'application/json',
+          }
+        }).then(function (response) {
+          window.location.href = 'index.html';
+          console.log(response);
+        })
+        .catch(function (error) {
+          alert("An error occured try again!");
+          console.log(error);
+        })
+      }
+  }
+});
