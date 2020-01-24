7ab5eaaab295ef60a50d3df3d9af073bdcd70ef7
pooyafekri79@gmail.com <po.fe.09134419188>
2020-01-24 13:39:07 +0330

    Add getUserRole

--- dashboard.js
+++ dashboard.js
@@ -1,8 +1,6 @@
 var powerOff = new Vue({
     el:'#power-off',
     data:{
-        name:'',
-        hello:''
     },
     methods:{
         logOut:function() {
@@ -10,48 +8,83 @@ var powerOff = new Vue({
             window.location.href = 'index.html';
         }
     }
-
+});
+var role = new Vue({
+    el:'#roles',
+    data:{
+        hasGraderRole:false,
+        hasProviderRole:false,
+        hasParticipant:false
+    },
+    methods:{
+    },
+    beforeMount : function(){
+    }
 });
 var dashboard = new Vue({
     el:'#username',
     data:{
         name:'',
-        hello:''
+        id:'',
+        datae:false
     },
     methods:{
     },
     beforeMount : function(){
-        var hello
+        var id
         axios.get('http://127.0.0.1:8080/api/v1/users/token',{
             headers: {
                 'Content-type': 'application/json',
                 'Authorization': document.cookie
             }
         }).then(function (response){
-            hello = response.data.id
-            console.log(hello)
-            this.hello = getUser(hello)
+            id = response.data
+            this.id = id
+            getUser(id)
+            role.getUser(id)
         }).catch(function (error) {
             console.log(error)
+            // window.location.href = 'index.html';
         })
     },
     mounted:function(){
-
-    },
-
+        
+    }
 });
-function getUser(hello){
-    // alert(hello)
-    axios.get('http://localhost:8080/api/v1/users/'+hello,{
+function getUser(id){
+    axios.get('http://localhost:8080/api/v1/users/'+id,{
         headers: {
             'Content-type': 'application/json',
             'Authorization': document.cookie
     }}).then( function (response) {
         dashboard.name = response.data.username,
-        alert(this.name);
         console.log(response.data.username)
     })
     .catch(function (error) {
-        console.log(hello)      
+        console.log(error);      
     })
+}
+function getUserRole(id){
+    alert(id);
+    axios.get('http://localhost:8080/api/v1/users/'+id+'/roles',{
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': document.cookie
+    }}).then( function (response) {
+        for (var key in response.data._embedded) {
+            if (response.data._embedded.hasOwnProperty(key)) {
+                if(key == "graders"){
+                    role.hasGraderRole = true;
+                }else if(key == "organizers"){
+                    role.hasProviderRole = true;
+                }else if(key == "participants"){
+                    role.hasParticipant =true;
+                }
+            }
+        }
+    })
+    .catch(function (error) {
+        console.log(error)      
+    })
+    
 }
