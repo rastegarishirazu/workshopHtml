c7cb1c7cbc2d1e5b451527dc6dd1cd07af3a48f8
pooyafekri79@gmail.com <po.fe.09134419188>
2020-01-25 09:01:07 +0330

    fixing role

--- script.js
+++ script.js
@@ -7,31 +7,31 @@ var quser = document.querySelector('.user');
 var qprovider = document.querySelector('.provider');
 var qparticipant = document.querySelector('.participant');
 
-
 function no_active(){
 	
-		grader[0].classList.remove('active');
-		user[0].classList.remove('active');
-		provider[0].classList.remove('active');
-	
+    grader[0].classList.remove('active');
+    user[0].classList.remove('active');
+    provider[0].classList.remove('active');
+    participant[0].classList.remove('active');
+
 }
 qgrader.addEventListener('click' , function(e){
-	e.preventDefault();
-	no_active();
-	grader[0].classList.add('active');
+e.preventDefault();
+no_active();
+grader[0].classList.add('active');
 })
 quser.addEventListener('click' , function(e){
-	e.preventDefault();
-	no_active();
-	user[0].classList.add('active');
+e.preventDefault();
+no_active();
+user[0].classList.add('active');
 })
 qprovider.addEventListener('click' , function(e){
-	e.preventDefault();
-	no_active();
-	provider[0].classList.add('active');
+e.preventDefault();
+no_active();
+provider[0].classList.add('active');
 })
 qparticipant.addEventListener('click' , function(e){
-	e.preventDefault();
-	no_active();
-	participant[0].classList.add('active');
+e.preventDefault();
+no_active();
+participant[0].classList.add('active');
 })
